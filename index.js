a5956c3a674c9500afef1fe80a18d0b622d437e2
roy-eugene049 <roy.eugenes000@gmail.com>
2022-09-26 15:11:25 +0300

    Working With Template Literals, Styling List Items

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-
-let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener")
+    myLeads.push(inputEl.value)
+    inputEl.value = " "
+    renderLeads()
 })
+
+// Log out the items in the myLeads array using a for loop 
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        /** 
+        console.log(myLeads[i])
+        //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+        const li = document.createElement("li")
+        li.textContent = myLeads[i]
+        ulEl.append(li)
+        */
+        //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    
+    ulEl.innerHTML = listItems
+}
+
