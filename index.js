8aa33023fc14ee89483c56a95e7a5599c09b5684
roy-eugene049 <roy.eugenes000@gmail.com>
2022-09-27 18:20:26 +0300

    Grabbing Current Tab URL

--- index.js
+++ index.js
@@ -4,12 +4,30 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
+
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener('click', function(){
+    //grab the URL of the current tab
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        
+
+        let activeTab = tabs[0]
+        let activeTabId = activeTab.id
+    })
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
 
 function render(leads) {
     let listItems = ""
