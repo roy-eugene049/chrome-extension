78e0de88895d2fd7d0a178b3b7f90ff5c306d375
roy-eugene049 <roy.eugenes000@gmail.com>
2022-09-27 13:47:12 +0300

    Saving to localStorage, Adding Delete Btn

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+// ["lead1", "lead2"] or null
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+console.log(leadsFromLocalStorage)
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    inputEl.value = " "
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
 })
-
-// Log out the items in the myLeads array using a for loop 
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        /** 
-        console.log(myLeads[i])
-        //ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-        const li = document.createElement("li")
-        li.textContent = myLeads[i]
-        ulEl.append(li)
-        */
-        //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
@@ -30,7 +32,5 @@ function renderLeads() {
             </li>
         `
     }
-    
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems  
 }
-
